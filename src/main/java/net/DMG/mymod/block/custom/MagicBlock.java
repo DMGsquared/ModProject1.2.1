cbdfbefb0ac04fc341ed57b2d3b794ca10fd474b
Andrew Chai <andrew.chai32@gmail.com>
2025-05-20 12:39:10 -0400

    Added custom tooltips

--- src/main/java/net/DMG/mymod/block/custom/MagicBlock.java
+++ src/main/java/net/DMG/mymod/block/custom/MagicBlock.java
@@ -2,6 +2,7 @@ package net.DMG.mymod.block.custom;
 
 import net.DMG.mymod.item.ModItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
@@ -12,10 +13,13 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+
+import java.util.List;
 
 public class MagicBlock extends Block {
     public MagicBlock(Properties properties) {
@@ -43,5 +47,11 @@ public class MagicBlock extends Block {
            player.hurt(pLevel.damageSources().dryOut(), 7);
         }
         super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, Item.TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+        pTooltipComponents.add(Component.translatable("tooltip.mymod.magic_block"));
+        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     }
 }
--- src/main/java/net/DMG/mymod/item/ModItems.java
+++ src/main/java/net/DMG/mymod/item/ModItems.java
@@ -3,11 +3,16 @@ package net.DMG.mymod.item;
 import net.DMG.mymod.MyMod;
 import net.DMG.mymod.item.custom.ChiselItem;
 import net.DMG.mymod.item.custom.FuelItem;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -16,8 +21,20 @@ public class ModItems {
     public static final RegistryObject<Item> OPTIMONIUM = ITEMS.register("optimonium", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> UNREFINED_OPTIMONIUM = ITEMS.register("unrefined_optimonium", ()-> new Item(new Item.Properties()));
     public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel", ()-> new ChiselItem(new Item.Properties().durability(128)));
-    public static final RegistryObject<Item> FRIED_EGG = ITEMS.register("fried_egg", ()-> new Item(new Item.Properties().food(ModFoodProperties.FRIED_EGG)));
-    public static final RegistryObject<Item> NEUTRON_STAR = ITEMS.register("neutron_star", () -> new FuelItem(new Item.Properties(), 32000));
+    public static final RegistryObject<Item> FRIED_EGG = ITEMS.register("fried_egg", ()-> new Item(new Item.Properties().food(ModFoodProperties.FRIED_EGG)){
+        @Override
+        public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+            pTooltipComponents.add(Component.translatable("tooltip.mymod.fried_egg"));
+            super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+        }
+    });
+    public static final RegistryObject<Item> NEUTRON_STAR = ITEMS.register("neutron_star", () -> new FuelItem(new Item.Properties(), 32000){
+        @Override
+        public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+            pTooltipComponents.add(Component.translatable("tooltip.mymod.neutron_star"));
+            super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+        }
+    });
 
 
     public static void register(IEventBus eventBus){
